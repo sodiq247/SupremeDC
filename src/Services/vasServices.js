f94bab56e62178845857006e9af9d1c3fc1e6071
Abdulrazaqsodiq <Abdulrazaqsodiq2015@gmail.com>
2023-09-11 09:08:47 +0100

    commit

--- src/Services/vasServices.js
+++ src/Services/vasServices.js
@@ -1,36 +1,31 @@
+/** @format */
+
 import axios from "axios";
 let baseUrl = "http://localhost:5030/api/v1/vas/";
 
 const vasServices = {
-    airTime: async (data) => {
-        let response = await axios
-        .post(`${baseUrl}airtime`, data);
-        return response;
-      },
-      dataBundle: async (data) => {
-        let response = await axios
-          .post(`${baseUrl}data`, data)
-          .catch((err) => {
-            console.log(err);
-          });
-        return response;
-      },
-      electric: async (data) => {
-        let response = await axios
-          .post(`${baseUrl}electric`, data)
-          .catch((err) => {
-            console.log(err);
-          });
-        return response;
-      },
-      cabletv: async (data) => {
-        let response = await axios
-          .post(`${baseUrl}cabletv`, data)
-          .catch((err) => {
-            console.log(err);
-          });
-        return response;
-      },
-}
+	airTime: async (data) => {
+		let response = await axios.post(`${baseUrl}airtime`, data);
+		return response;
+	},
+	dataBundle: async (data) => {
+		let response = await axios.post(`${baseUrl}data`, data).catch((err) => {
+			console.log(err);
+		});
+		return response;
+	},
+	electric: async (data) => {
+		let response = await axios.post(`${baseUrl}electric`, data).catch((err) => {
+			console.log(err);
+		});
+		return response;
+	},
+	cabletv: async (data) => {
+		let response = await axios.post(`${baseUrl}cabletv`, data).catch((err) => {
+			console.log(err);
+		});
+		return response;
+	},
+};
 
 export default vasServices;
